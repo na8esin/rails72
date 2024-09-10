ebdd1a3ce88a72ad7389f74c4aeb3d8755052cc5
Takayuki Watanabe <watanabe@sikmi.com>
2024-09-10 15:40:57 +0900

    Previewing Emailsを試してみた

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  def welcome_email
+    @user = Data.define(:email).new("watanabe@example.com")
+    @url  = "http://example.com/login"
+    mail(to: @user.email,
+         subject: "Welcome to My Awesome Site")
+  end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,3 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def welcome_email
+    UserMailer.with(user: { id: 1, email: "watanabe@example.com" }).welcome_email
+  end
 end
