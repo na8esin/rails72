bce892f66ac2fcd4e7387cd29bdd71d5d94471f0
Takayuki Watanabe <watanabe@sikmi.com>
2024-09-26 15:55:04 +0900

    無駄を省いたクラスメソッドのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,16 +15,12 @@ RSpec.describe User, type: :model do
   # https://stackoverflow.com/questions/38445625/rspec-class-spy-on-rails-mailer
   context '.new' do
     it 'say Good morning' do
-      double = class_spy(ExternalApi).as_stubbed_const
-      mock_instance = instance_double("ExternalApi")
-
-      allow(ExternalApi).to receive(:new) { mock_instance }
-
-      allow(mock_instance).to receive(:fetch) { '' }
+      # これだけで、partial doubleになるし、fetchメソッドもそのまま
+      allow(ExternalApi).to receive(:new).and_call_original
 
       described_class.new.fetch_external_with_initial_value
 
-      expect(double).to have_received(:new).with(greet: "Good morning")
+      expect(ExternalApi).to have_received(:new).with(greet: "Good morning")
     end
   end
 end
