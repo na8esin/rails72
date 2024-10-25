c34e356fdefe1d3301829f79533dd765d16404a0
Takayuki Watanabe <watanabe@sikmi.com>
2024-10-25 22:37:22 +0900

    コメント修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 # あるクラスが依存しているクラスのnewを通過する引数を検査したい時のサンプル
 # newはクラスメソッドなので、クラスメソッドをスタブするのどうやるんだっけ？というのがスタート
-#
-# 前提知識:
-#   pure test doubleとpartial doubleと言うものがある
-#   そして、`and_call_original` is only available on a partial double.
-#
-# 断片的にコードをブログに載せるときは、described_classだとわからないので、あまり使ってない
 
 RSpec.describe User, type: :model do
   fixtures :users
@@ -29,7 +23,6 @@ RSpec.describe User, type: :model do
   it 'ExternalApiのnewとfetchを補った後で、as_stubbed_constする' do
     class_double(ExternalApi, new: instance_double(ExternalApi, fetch: 'hello')).as_stubbed_const
 
-    # #<ClassDouble(ExternalApi) (anonymous)> received unexpected message :new with ({:greet=>"Good morning"})
     index
 
     expect(ExternalApi).to have_received(:new).with(greet: "Good morning")
